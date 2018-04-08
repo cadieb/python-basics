7dacbe241e5be3b7d9cd789dd489df95e1596074
cadie <cadie@dreamfactoryagency.com>
2018-04-07 21:47:32 -0400

    Booleans notes

---
+++ logic-in-python/booleans.py
@@ -0,0 +1,4 @@
+# true (1) false (0). bool(___) function tests things to ssee if they are true or false.Full things are true, empty things are false. Nothingness is a value (known as none) which is false.
+
+# Comparison
+# == is equal to, != is does not equal. Python does not have '==='. Is is a special operation. Avoid using is most of the time. It does not work the same as equals to. It is the best way to check if a variable is none!
